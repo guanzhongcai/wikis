45d469a9d7d89d7b234d526a0df4fa1a9c5df1b1
guanzhongcai <zhongcai.guan@united-imaging.com>
2021-06-26 20:56:36 +0800

    go generate can run

--- langs/golang/go-concurrency/coolshell/go_generation/gen.sh
+++ langs/golang/go-concurrency/coolshell/go_generation/gen.sh
@@ -6,12 +6,11 @@ SRC_FILE=${1}
 PACKAGE=${2}
 TYPE=${3}
 DES=${4}
-
-# uppercase the first char
+#uppcase the first char
 PREFIX="$(tr '[:lower:]' '[:upper:]' <<< ${TYPE:0:1})${TYPE:1}"
 
 DES_FILE=$(echo ${TYPE}| tr '[:upper:]' '[:lower:]')_${DES}.go
 
 sed 's/PACKAGE_NAME/'"${PACKAGE}"'/g' ${SRC_FILE} | \
     sed 's/GENERIC_TYPE/'"${TYPE}"'/g' | \
-    sed 's/GENERIC_TYPE/'"${TYPE}"'/g' > ${DES_FILE}
+    sed 's/GENERIC_NAME/'"${PREFIX}"'/g' > ${DES_FILE}
--- langs/golang/go-concurrency/coolshell/go_generation/generate.go
+++ langs/golang/go-concurrency/coolshell/go_generation/generate.go
@@ -2,16 +2,16 @@ package go_generation
 
 import "fmt"
 
-//go:generate ./gen.sh ./template/container.tmp.go gen uint32 container
+//go:generate ./gen.sh ./template/container.tmp.go go_generation uint32 container
 func generateUin32Example()  {
-	var u int32 = 42
+	var u uint32 = 42
 	c := NewUint32Container()
 	c.Put(u)
 	v := c.Get()
 	fmt.Printf("generateUin32Example: %d (%T)\n", v, v)
 }
 
-//go:generate ./gen.sh ./template/container.tmp.go gen string container
+//go:generate ./gen.sh ./template/container.tmp.go go_generation string container
 func generateStringExample()  {
 	var s string = "Hello"
 	c := NewStringContainer()
